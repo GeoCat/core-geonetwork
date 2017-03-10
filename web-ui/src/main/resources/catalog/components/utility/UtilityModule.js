6692e933da0f5272f6115c502909a224d163114f
Pascallike <antonio.cerciello@gmail.com>
2017-03-10 12:30:17 +0100

    angular filter for PDC

--- web-ui/src/main/resources/catalog/components/utility/UtilityModule.js
+++ web-ui/src/main/resources/catalog/components/utility/UtilityModule.js
@@ -111,5 +111,46 @@
           }
           return input.split(splitChar)[splitIndex];
         }
+      })
+      
+      /* PDC filter for the home page */
+.filter('PDCFilter', function() {
+        return function(input, noPDC) {   
+
+          var out = [];
+
+          angular.forEach(input, function(metadata) {
+
+            var cat = metadata.category;
+
+            if (angular.isArray(cat)) {
+              if(noPDC) {
+                if(cat.indexOf('PDC') === -1) {
+                  out.push(metadata);
+                }
+              } else {
+                if(cat.indexOf('PDC') !== -1) {
+                  out.push(metadata);
+                }
+              }
+
+            } else if(angular.isString(cat)) {              
+
+              if(noPDC && cat != 'PDC') {
+                out.push(metadata);
+              } else if(!noPDC && cat === 'PDC') {
+                out.push(metadata);
+              }
+            } else {
+              if(noPDC) {
+                out.push(metadata);
+              } 
+            }
+
+          });
+
+          return out;     
+        }
       });
+  
 })();
